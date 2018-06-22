bbd257704032492fb8b38d13e6faf1cdb6870a18
Lee Stewart <lee.stewart@starlingbank.com>
2018-06-22 17:05:00 +0100

    Hamming WIP (not working)

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -3,10 +3,19 @@ class Hamming {
     Hamming(String leftStrand, String rightStrand) {
       if (leftStrand.length() != rightStrand.length()) {
         throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+      }
     }
 
     int getHammingDistance() {
 
+      int hammingDistance = 0;
+
+      for (char c : leftString.toCharArray()) {
+          if (c != rightStrand.toCharArray()[leftStrand.toCharArray().indexOf(c)]) {
+              hammingDistance++;
+          }
+      }
+      return hammingDistance;
     }
 
 }
