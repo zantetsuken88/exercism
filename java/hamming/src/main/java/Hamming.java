73bee1cad92ad304cb4a29f2708220d84f629c76
Lee Stewart <lee.stewart@starlingbank.com>
2018-07-24 17:35:12 +0100

    Final Hamming solution + new additional exercise

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,28 +1,29 @@
 class Hamming {
 
-    private String leftStrand;
-    private String rightStrand;
+  private String leftStrand;
+  private String rightStrand;
 
-    Hamming(String leftStrand, String rightStrand) {
-        this.leftStrand = leftStrand;
-        this.rightStrand = rightStrand;
-        if (leftStrand.length() != rightStrand.length()) {
-            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        }
+  Hamming(String leftStrand, String rightStrand) {
+    if (leftStrand.length() != rightStrand.length()) {
+      throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
     }
+    this.leftStrand = leftStrand;
+    this.rightStrand = rightStrand;
 
-    int getHammingDistance() {
+  }
 
-        int hammingDistance = 0;
-        int count = 0;
+  int getHammingDistance() {
 
-        for (char c : leftStrand.toCharArray()) {
-            if (c != rightStrand.toCharArray()[count]) {
-                hammingDistance++;
-            }
-            count++;
-        }
-        return hammingDistance;
+    int hammingDistance = 0;
+    int count = 0;
+
+    for (char c : leftStrand.toCharArray()) {
+      if (c != rightStrand.charAt(count)) {
+        hammingDistance++;
+      }
+      count++;
     }
+    return hammingDistance;
+  }
 
 }
