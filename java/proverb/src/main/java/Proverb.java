c57e9507bc6a5c7d8533e1437c45d0c5542fdee9
Lee Stewart <luigizan88@gmail.com>
2018-08-01 09:44:31 +0100

    proverb solution submitted

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Proverb {
 
     private String[] words;
@@ -8,9 +10,22 @@ class Proverb {
     }
 
     String recite() {
+
         StringBuilder proverb = new StringBuilder();
 
+        if (words.length != 0) {
 
+            for (String word : words) {
+                if ((Arrays.asList(words).indexOf(word)) <= (words.length - 2)) {
+                    String lost = words[(Arrays.asList(words).indexOf(word)) + 1];
+                    proverb.append("For want of a " + word + " the " + lost + " was lost.\n");
+                }
+            }
+
+            proverb.append("And all for the want of a " + words[0] + ".");
+
+        }
+        return proverb.toString();
     }
 
 }
