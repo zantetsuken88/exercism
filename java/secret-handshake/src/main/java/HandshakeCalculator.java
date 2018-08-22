aa4b9bd4ae05183a0b6eb7bdc2e089865080f49f
Lee Stewart <lee.stewart@starlingbank.com>
2018-08-22 10:55:07 +0100

    secret handshake solution submitted

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -5,27 +5,29 @@ import java.util.List;
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        List<Signal> list = new ArrayList<>();
-
-        StringBuilder binary = new StringBuilder(Integer.toBinaryString(number));
+      StringBuilder binary = new StringBuilder(Integer.toBinaryString(number));
+      List<Signal> list = new ArrayList<>();
         binary.reverse();
-
-        if(binary.charAt(0) == '1') {
+        if(getSecretSignal(1, 0, binary)) {
             list.add(Signal.WINK);
         }
-        if ((binary .length() >= 2) && (binary.charAt(1) == '1')) {
+        if (getSecretSignal(2, 1, binary)) {
             list.add(Signal.DOUBLE_BLINK);
         }
-        if ((binary .length() >= 3) && (binary.charAt(2) == '1')) {
+        if (getSecretSignal(3, 2, binary)) {
             list.add(Signal.CLOSE_YOUR_EYES);
         }
-        if ((binary .length() >= 4) && (binary.charAt(3) == '1')) {
+        if (getSecretSignal(4, 3, binary)) {
             list.add(Signal.JUMP);
         }
-        if ((binary .length() >= 5) && (binary.charAt(4) == '1')) {
+        if (getSecretSignal(5,4, binary)) {
             Collections.reverse(list);
         }
         return list;
+    }
+
+    private boolean getSecretSignal(int binaryLength, int binaryChar, StringBuilder binary) {
+        return ((binary.length() >= binaryLength) && binary.charAt(binaryChar) == '1');
     }
 
 }
